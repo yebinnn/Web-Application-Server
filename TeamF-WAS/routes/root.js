d8f227d3f7a83430677fc35f0780244f9558c00b
yebinnn <926167912@gmail.com>
2023-01-19 23:10:51 +0900

    project2: hhelloworld-was 생성

--- TeamF-WAS/routes/root.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-module.exports = async function (fastify, opts) {
-  fastify.get('/', async function (request, reply) {
-    console.log(process.env.MONGO_HOSTNAME)
-    console.log(process.env.MONGO_USERNAME)
-    return { root: true, host: process.env.MONGO_HOSTNAME, username: process.env.MONGO_USERNAME }
-  })
-}
