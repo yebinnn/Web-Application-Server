b21261f736fd8640f3ffaaea78a8411390fef5e3
yebinnn <926167912@gmail.com>
2023-01-20 18:51:17 +0900

    mongodb파일 생성 및 index.js/root.js 내용 변경완료

--- helloworld-was/routes/root.js
+++ helloworld-was/routes/root.js
@@ -2,6 +2,8 @@
 
 module.exports = async function (fastify, opts) {
   fastify.get('/', async function (request, reply) {
-    return { root: false }
+    console.log(process.env.MONGO_HOSTNAME)
+    console.log(process.env.MONGO_USERNAME)
+    return { root: true, host: process.env.MONGO_HOSTNAME, username: process.env.MONGO_USERNAME }
   })
 }
