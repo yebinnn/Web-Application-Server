0a286e998337c6689fa7195693186a0c2bc563a6
yebinnn <926167912@gmail.com>
2023-01-25 22:30:34 +0900

    index.js 파일 생성

---
+++ helloworld-was/routes/api/orders/index.js
@@ -0,0 +1,24 @@
+'use strict'
+
+module.exports = async function (fastify, opts) {
+  
+    fastify.get('/', async function (req, reply) {
+    const database = fastify.client.db("baedal")
+    const result = await database.collection("orders").find().toArray()
+    
+    reply 
+    .code(200)
+    .header('Content-type','application/json')
+    .send(result)
+  })
+  fastify.get('/:_id', async function (req, reply) {
+    const database = fastify.client.db("baedal")
+    const ObjectID = require('mongodb')
+    const result = await database.collection("orders").find({_id: new ObjectID(req.params._id)}).toArray()
+    
+    reply 
+    .code(200)
+    .header('Content-type','application/json')
+    .send(result)
+  })
+}
--- helloworld-was/routes/api/restaurants/index.js
+++ helloworld-was/routes/api/restaurants/index.js
@@ -2,24 +2,11 @@
 
 module.exports = async function (fastify, opts) {
   fastify.get('/', async function (req, reply) {
-    reply.send([
-      {
-        "name": "용다방",
-        "menu": [
-          {
-            "name": "애플 시나몬 에이드",
-            "price": 6500,
-            "duration": 5
-          },
-          {
-            "name": "카페모카",
-            "price": 6000,
-            "duration": 5
-          }
-        ],
-        "address": "서울시 마포구 양화로 1111",
-        "rating": 4.5
-      }
-    ])
+    const database = fastify.client.db("baedal")
+    const result = await database.collection("restaurants").find().toArray()
+    reply 
+    .code(200)
+    .header('Content-type','application/json')
+    .send(result)
   })
 }
